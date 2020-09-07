1b2f860305ae33c32678c7516fd42904b587c6a5
francis <chicano@lcc.uma.es>
2020-09-07 20:12:38 +0200

    Actualizado

--- src/main/java/es/uma/informatica/sii/jms/websocket/MonitorWebsocket.java
+++ src/main/java/es/uma/informatica/sii/jms/websocket/MonitorWebsocket.java
@@ -1,12 +1,13 @@
 package es.uma.informatica.sii.jms.websocket;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.logging.Logger;
 
 import javax.enterprise.event.Observes;
+import javax.inject.Named;
 import javax.websocket.CloseReason;
 import javax.websocket.EndpointConfig;
 import javax.websocket.OnClose;
@@ -20,20 +21,20 @@ import es.uma.informatica.sii.jms.mdb.Alta;
 
 @ServerEndpoint("/monitor")
 public class MonitorWebsocket {
-	
 	private static final Logger LOGGER = Logger.getLogger(MonitorWebsocket.class.getCanonicalName());
-	private Session currentSession;
+	private static Set<Session> sesiones = Collections.synchronizedSet(new HashSet<>());
 	
 	@OnOpen
 	public void open(Session session,
 	                 EndpointConfig conf) { 
-		currentSession = session;
+		sesiones.add(session);
 		LOGGER.info("Se abre conexión de websocket");
 	}
 
 	@OnClose
 	public void close(Session session, CloseReason reason) { 
 		LOGGER.info("Se acierra conexión de websocket");
+		sesiones.remove(session);
 	}
 	
 	@OnError
@@ -46,9 +47,15 @@ public class MonitorWebsocket {
 		LOGGER.info("Llega mensaje del cliente: "+msg);
 	}
 	
-	private void notificar(@Observes @Alta String cuenta) {
+	public static void notificar(@Observes @Alta String cuenta) {
+		LOGGER.info("Notifican al Websocket");
 		try {
-			currentSession.getBasicRemote().sendText(cuenta);
+			synchronized (sesiones) {
+				for (Session sesion: sesiones) {
+					LOGGER.info("Enviado mensaje al otro lado del WebSocket sobre "+cuenta);
+					sesion.getBasicRemote().sendText(cuenta);
+				}
+			}
 			LOGGER.info("Notificada el alta de "+cuenta);
 		} catch (IOException e) {
 			LOGGER.severe(e.getMessage());
